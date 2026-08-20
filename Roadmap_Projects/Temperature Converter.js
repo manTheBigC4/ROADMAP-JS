7dc8071bb145ae1f849dd18eba2fed0dcf6996a6
manTheBigC4 <bzoom123456789@gmail.com>
2026-08-20 20:42:38 +0800

    change

---
+++ Roadmap_Projects/Temperature Converter.js
@@ -0,0 +1,17 @@
+function celsiusToFarenheit(celsius) {
+    return celsius * (9 / 5) + 32;
+}
+
+function farenheitToCelsius(farenheit) {
+    return (farenheit - 32) / (9 / 5);
+}
+
+function formatTemperature(value, unit) {
+    if (typeof unit !== "string") {
+        return;
+    }
+
+    return `${value} ${unit.toUpperCase()}`;
+}
+
+console.log(formatTemperature(celsiusToFarenheit(20), "f"));
